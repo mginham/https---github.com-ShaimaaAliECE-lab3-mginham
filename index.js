bb40726c5ee558167b4a460a1dc4d6860a6cfae8
Megan Ginham <45317731+mginham@users.noreply.github.com>
2021-11-20 19:59:56 -0500

    Added some styling

--- index.js
+++ index.js
@@ -104,19 +104,55 @@ app.get("/admin", (req, res) => {
     conn.connect(); // Establish the connection
 
     // Get input from the admin
-    let text = `<form action='/edit-timeslots'>
-                    <h2> Enter hours to be surveyed (in 24 hour format) <h2>
-                    <input name='slotTime1', placeholder="Hour"/>
-                    <input name='slotTime2', placeholder="Hour"/>
-                    <input name='slotTime3', placeholder="Hour"/>
-                    <input name='slotTime4', placeholder="Hour"/>
-                    <input name='slotTime5', placeholder="Hour"/>
-                    <input name='slotTime6', placeholder="Hour"/>
-                    <input name='slotTime7', placeholder="Hour"/>
-                    <input name='slotTime8', placeholder="Hour"/>
-                    <input name='slotTime9', placeholder="Hour"/>
-                    <input name='slotTime10', placeholder="Hour"/>
-                    <input type="submit" value="Submit">`
+    let text = `<table>
+                    <tr>
+                        <form action='/edit-timeslots'>
+                        <h2> Enter hours to be surveyed (in 24 hour format; do not include minutes) <h2>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 1:</td>
+                        <td><input name='slotTime1', placeholder="Hour 1"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 2:</td>
+                        <td><input name='slotTime2', placeholder="Hour 2"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 3:</td>
+                        <td><input name='slotTime3', placeholder="Hour 3"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 4:</td>
+                        <td><input name='slotTime4', placeholder="Hour 4"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 5:</td>
+                        <td><input name='slotTime5', placeholder="Hour 5"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 6:</td>
+                        <td><input name='slotTime6', placeholder="Hour 6"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 7:</td>
+                        <td><input name='slotTime7', placeholder="Hour 7"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 8:</td>
+                        <td><input name='slotTime8', placeholder="Hour 8"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 9:</td>
+                        <td><input name='slotTime9', placeholder="Hour 9"/></td>
+                    </tr>
+                    <tr>
+                        <td>Timeslot 10:</td>
+                        <td><input name='slotTime10', placeholder="Hour 10"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Submit"></td>
+                    </tr>
+                </table>`
 
     res.send(text);
 
@@ -140,7 +176,7 @@ app.get('/calendar', (req, res) => {
                 <p>Times are displayed in 24 hrs</p>
                 <table>   
                     <tr>
-                        <th>Guest Name</th>
+                        <th style="text-align:left">Guest Name</th>
                         <th>` + slotTimes[0] + `:00</th>
                         <th>` + slotTimes[1] + `:00</th>
                         <th>` + slotTimes[2] + `:00</th>
@@ -155,33 +191,33 @@ app.get('/calendar', (req, res) => {
                     <tr>
                         <form action='/add-availability'>
                         <td><input name='guestName'/></td>
-                        <td><input name='timeslot1', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot2', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot3', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot4', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot5', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot6', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot7', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot8', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot9', type="checkbox", value = "1" /></td>
-                        <td><input name='timeslot10', type="checkbox", value = "1" /></td>
-                        <td><input type="submit" value="Submit"></td>
+                        <td style="text-align:center"><input name='timeslot1', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot2', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot3', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot4', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot5', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot6', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot7', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot8', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot9', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input name='timeslot10', type="checkbox", value = "1" /></td>
+                        <td style="text-align:center"><input type="submit" value="Submit"></td>
                     </tr>
             `;
 
             for (t of guestInfo) {
                 text += '<tr>'
                     + '<td>' + t.guestName + '</td>' 
-                    + '<td>' + t.timeslot1 + '</td>' 
-                    + '<td>' + t.timeslot2 + '</td>' 
-                    + '<td>' + t.timeslot3 + '</td>' 
-                    + '<td>' + t.timeslot4 + '</td>' 
-                    + '<td>' + t.timeslot5 + '</td>' 
-                    + '<td>' + t.timeslot6 + '</td>' 
-                    + '<td>' + t.timeslot7 + '</td>' 
-                    + '<td>' + t.timeslot8 + '</td>' 
-                    + '<td>' + t.timeslot9 + '</td>' 
-                    + '<td>' + t.timeslot10 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot1 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot2 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot3 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot4 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot5 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot6 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot7 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot8 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot9 + '</td>' 
+                    + '<td style="text-align:center">' + t.timeslot10 + '</td>' 
                     + '</tr>';
             }
 
