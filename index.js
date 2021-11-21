6210917c2f5887a3fc1a821f5b17adf70fe5b22e
Megan Ginham <45317731+mginham@users.noreply.github.com>
2021-11-20 19:36:37 -0500

    Added admin ability to change times

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const newConn = require('./DBConnection'); // Import the connection details
 
+//List of timeslot hours
+const slotTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
 // Creates the routing needed to serve the static files (can now load the files that are in the public directory)
 app.use(express.static('static'))
 
@@ -80,185 +83,110 @@ app.get("/add-availability", (req, res) => {
 app.get("/edit-timeslots", (req, res) => {
     let conn = newConn(); // Create the connection
     conn.connect(); // Establish the connection
-    
-    conn.query(`ALTER TABLE Availability change timeslot1 '${req.query.timeslot1} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
 
-    conn.query(`ALTER TABLE Availability change timeslot2 '${req.query.timeslot2} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot3 '${req.query.timeslot3} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot4 '${req.query.timeslot4} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot5 '${req.query.timeslot5} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot6 '${req.query.timeslot6} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot7 '${req.query.timeslot7} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot8 '${req.query.timeslot8} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot9 '${req.query.timeslot9} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot10 '${req.query.timeslot10} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
-
-    conn.query(`ALTER TABLE Availability change timeslot2 '${req.query.timeslot2} tinytint)`, 
-        (err, rows, fields) => {
-            res.redirect('/admin');
-        }
-    );
+    slotTimes[0] = req.query.slotTime1;
+    slotTimes[1] = req.query.slotTime2;
+    slotTimes[2] = req.query.slotTime3;
+    slotTimes[3] = req.query.slotTime4;
+    slotTimes[4] = req.query.slotTime5;
+    slotTimes[5] = req.query.slotTime6;
+    slotTimes[6] = req.query.slotTime7;
+    slotTimes[7] = req.query.slotTime8;
+    slotTimes[8] = req.query.slotTime9;
+    slotTimes[9] = req.query.slotTime10;
 
     conn.end(); // End the connection
 })
 
+// Open the admin page
 app.get("/admin", (req, res) => {
     let conn = newConn(); // Create the connection
     conn.connect(); // Establish the connection
 
+    // Get input from the admin
     let text = `<form action='/edit-timeslots'>
-    <h2> Please Insert the Available Availability (10 minute slots)<h2>
-    <h4> Available Availability should not be changed after user data is submitted <h4>
-    <input name='timeslot1', placeholder="HH:MM"/>
-    <input name='timeslot2', placeholder="HH:MM"/>
-    <input name='timeslot3', placeholder="HH:MM"/>
-    <input name='timeslot4', placeholder="HH:MM"/>
-    <input name='timeslot5', placeholder="HH:MM"/>
-    <input name='timeslot6', placeholder="HH:MM"/>
-    <input name='timeslot7', placeholder="HH:MM"/>
-    <input name='timeslot8', placeholder="HH:MM"/>
-    <input name='timeslot9', placeholder="HH:MM"/>
-    <input name='timeslot10', placeholder="HH:MM"/>
-    <input type="submit" value="Submit">
-
-    <h2>Guest View:</h2>
-    <a href='/calendar'>See Calendar</a>`
+                    <h2> Enter hours to be surveyed (in 24 hour format) <h2>
+                    <input name='slotTime1', placeholder="Hour"/>
+                    <input name='slotTime2', placeholder="Hour"/>
+                    <input name='slotTime3', placeholder="Hour"/>
+                    <input name='slotTime4', placeholder="Hour"/>
+                    <input name='slotTime5', placeholder="Hour"/>
+                    <input name='slotTime6', placeholder="Hour"/>
+                    <input name='slotTime7', placeholder="Hour"/>
+                    <input name='slotTime8', placeholder="Hour"/>
+                    <input name='slotTime9', placeholder="Hour"/>
+                    <input name='slotTime10', placeholder="Hour"/>
+                    <input type="submit" value="Submit">`
 
     res.send(text);
 
     conn.end(); // End the connection
 })
 
+// Open the guest calendar page
 app.get('/calendar', (req, res) => {
     let conn = newConn(); // Create the connection
     conn.connect(); // Establish the connection
 
-    let text = ``;
-    let timeslots;
+    let text = ``; // Text to be displayed
+    let guestInfo; // Array of guest info
 
+    // Receive input from the user regarding their availability
     conn.query('SELECT * FROM Availability', 
         (err, rows, fields) => {
-            timeslots = rows;
+            guestInfo = rows;
 
-            text += 
-                `<table>   
+            text += `
+                <p>Times are displayed in 24 hrs</p>
+                <table>   
                     <tr>
-                        <th>guestName</th>
-                        <th>Timeslot 1</th>
-                        <th>Timeslot 2</th>
-                        <th>Timeslot 3</th>
-                        <th>Timeslot 4</th>
-                        <th>Timeslot 5</th>
-                        <th>Timeslot 6</th>
-                        <th>Timeslot 7</th>
-                        <th>Timeslot 8</th>
-                        <th>Timeslot 9</th>
-                        <th>Timeslot 10</th>
+                        <th>Guest Name</th>
+                        <th>` + slotTimes[0] + `:00</th>
+                        <th>` + slotTimes[1] + `:00</th>
+                        <th>` + slotTimes[2] + `:00</th>
+                        <th>` + slotTimes[3] + `:00</th>
+                        <th>` + slotTimes[4] + `:00</th>
+                        <th>` + slotTimes[5] + `:00</th>
+                        <th>` + slotTimes[6] + `:00</th>
+                        <th>` + slotTimes[7] + `:00</th>
+                        <th>` + slotTimes[8] + `:00</th>
+                        <th>` + slotTimes[9] + `:00</th>
                     </tr>
                     <tr>
-                    <form action='/add-availability'>
-                    <td><input name='guestName'/></td>
-                    <td><input name='timeslot1', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot2', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot3', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot4', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot5', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot6', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot7', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot8', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot9', type="checkbox", value = "1" /></td>
-                    <td><input name='timeslot10', type="checkbox", value = "1" /></td>
-                    <td><input type="submit" value="Submit"></td>
+                        <form action='/add-availability'>
+                        <td><input name='guestName'/></td>
+                        <td><input name='timeslot1', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot2', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot3', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot4', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot5', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot6', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot7', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot8', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot9', type="checkbox", value = "1" /></td>
+                        <td><input name='timeslot10', type="checkbox", value = "1" /></td>
+                        <td><input type="submit" value="Submit"></td>
                     </tr>
-                    `;
+            `;
 
-            for (p of timeslots) {
-                text += '<tr>';
-                text += '<td>';
-                text += p.guestName;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot1;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot2;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot3;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot4;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot5;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot6;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot7;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot8;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot9;
-                text += '</td>';
-                text += '<td>';
-                text += p.timeslot10;
-                text += '</td>';
-                text += '</tr>';
-                
+            for (t of guestInfo) {
+                text += '<tr>'
+                    + '<td>' + t.guestName + '</td>' 
+                    + '<td>' + t.timeslot1 + '</td>' 
+                    + '<td>' + t.timeslot2 + '</td>' 
+                    + '<td>' + t.timeslot3 + '</td>' 
+                    + '<td>' + t.timeslot4 + '</td>' 
+                    + '<td>' + t.timeslot5 + '</td>' 
+                    + '<td>' + t.timeslot6 + '</td>' 
+                    + '<td>' + t.timeslot7 + '</td>' 
+                    + '<td>' + t.timeslot8 + '</td>' 
+                    + '<td>' + t.timeslot9 + '</td>' 
+                    + '<td>' + t.timeslot10 + '</td>' 
+                    + '</tr>';
             }
+
             text += '</table>';
+
             res.send(text);
         })
 
@@ -266,16 +194,3 @@ app.get('/calendar', (req, res) => {
 });
 
 app.listen(80); // Make server listen to port 80 to avoid firewall configuration 
-
-/*
-// Add a booking
-app.get('/add-booking', (req, res) => {
-    let conn = newConn(); // Create the connection
-    conn.connect(); // Establish the connection
-
-    //Retrieve value from the input and insert them into the table
-    conn.query(`insert into Availability values ('${req.query.desc}')`, (err, rows, fields) => {});
-
-    conn.end(); // End the connection
-})
-*/
